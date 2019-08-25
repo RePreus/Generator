ca80729613a88aadfce9483c888d6bbd77dec7bd
Preus <panma@vp.pl>
2019-08-25 21:03:41 +0200

    Removed unnecessary using statement

--- Generator.Application/Handlers/PayloadHandler.cs
+++ Generator.Application/Handlers/PayloadHandler.cs
@@ -2,7 +2,6 @@ using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Generator.Application.Extensions;
 using Generator.Application.Models;
 using Generator.Application.Persistence;
 using MediatR;
