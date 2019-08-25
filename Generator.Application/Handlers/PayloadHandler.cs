fbac48c28c51d872fbb1d9d1eb2eeaf1ddd8105e
Preus <panma@vp.pl>
2019-08-25 21:03:25 +0200

    Changed location of GetRandomIds method

--- Generator.Application/Handlers/PayloadHandler.cs
+++ Generator.Application/Handlers/PayloadHandler.cs
@@ -21,10 +21,22 @@ namespace Generator.Application.Handlers
         public Task<Payload> Handle(ReceivedName tableName, CancellationToken token)
         {
             var count = context.Pictures.Count();
-            Tuple<int, int> ids = count.GetRandomIds();
+            Tuple<int, int> ids = GetRandomIds(count);
             var picture1 = context.Pictures.Find(ids.Item1).Image;
             var picture2 = context.Pictures.Find(ids.Item2).Image;
             return Task.FromResult(new Payload(ids.Item1, picture1, ids.Item2, picture1));
+        }
+
+        public static Tuple<int, int> GetRandomIds(int range)
+        {
+            Random rand = new Random();
+            int id1 = rand.Next(1, range + 1), id2;
+            do
+            {
+                id2 = rand.Next(1, range + 1);
+            }
+            while (id1 == id2);
+            return Tuple.Create(id1, id2);
         }
     }
 }
