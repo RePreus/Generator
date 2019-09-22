d2efa9c5fabdc0456a25da6e5c268e4973e3fff3
Preus <panma@vp.pl>
2019-09-22 17:05:01 +0200

    Change Choice class

---
+++ Generator.Domain/Entities/Choice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Generator.Domain.Entities
+{
+    public class Choice
+    {
+        public Choice(Guid pictureAId, Guid pictureBId, Guid userChoiceId)
+        {
+            PictureAId = pictureAId;
+            PictureBId = pictureBId;
+            UserChoiceId = userChoiceId;
+        }
+        public Guid PictureAId { get; }
+
+        public string PictureA { get; set; }
+
+        public Guid PictureBId { get; }
+
+        public string PictureB { get; set; }
+
+        public Guid UserChoiceId { get; }
+    }
+}
