a3374135a2748a2f8c43715148c3431550d57c36
RePreus <panma@vp.pl>
2019-10-05 13:59:14 +0200

    Bug fix (#3)

--- Generator.Application/Migrations/20191001113005_userchoice.Designer.cs
+++ Generator.Application/Migrations/20191001113005_userchoice.Designer.cs
@@ -4,14 +4,16 @@ using Generator.Application.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Generator.Application.Migrations
 {
     [DbContext(typeof(GeneratorContext))]
-    partial class GeneratorContextModelSnapshot : ModelSnapshot
+    [Migration("20191001113005_userchoice")]
+    partial class userchoice
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
